06685a9bdd6a88f27f6a6bebb2021a5885601ca2
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2023-09-23 14:27:55 +0300

    complete_01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  currentColor: document.querySelector('body'),
+};
+let timerId = null;
+refs.btnStart.addEventListener('click', changeBackColor);
+refs.btnStop.addEventListener('click', stopChangColor);
 
+function changeBackColor() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    refs.currentColor.style.backgroundColor = randomColor;
+
+    console.log('Second log');
+  }, 1000);
+}
+function stopChangColor() {
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
